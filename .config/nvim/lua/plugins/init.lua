0ffe904e0ca6993609e53a86d1299a6934ffd54b
Konrad Guzek <konrad@guzek.uk>
2025-12-27 04:48:51 +0100

    config(nvim): replace git-blame with gitsigns

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -25,4 +25,14 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 0,
+      }
+    }
+  }
 }
--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,19 +1,17 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    cmd = { "ToggleTerm" },
-    config = function()
-      require("toggleterm").setup {
-        size = 16, -- default height
-        -- open_mapping = [[<c-\>]], -- Ctrl+\ toggles
-        hide_numbers = true,
-        shade_filetypes = {},
-        shade_terminals = true,
-        start_in_insert = true,
-        persist_size = true,      -- keeps terminal size
-        direction = "horizontal", -- can also be 'vertical' or 'float'
-      }
-    end
-  }
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  cmd = { "ToggleTerm" },
+  config = function()
+    require("toggleterm").setup {
+      size = 16,   -- default height
+      -- open_mapping = [[<c-\>]], -- Ctrl+\ toggles
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      start_in_insert = true,
+      persist_size = true,        -- keeps terminal size
+      direction = "horizontal",   -- can also be 'vertical' or 'float'
+    }
+  end
 }
