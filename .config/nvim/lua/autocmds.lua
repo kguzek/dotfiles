dc1c3b1e0e6c19a64d09b86b647c4696bd9292f2
Konrad Guzek <konrad@guzek.uk>
2026-01-19 23:06:46 +0100

    restart prettierd on config change

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,1 +1,9 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  group = vim.api.nvim_create_augroup("RestartPrettierd", { clear = true }),
+  pattern = "*prettier*",
+  callback = function()
+    vim.fn.system "prettierd restart"
+  end,
+})
