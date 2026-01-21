a3d5e6daed44545e502fe2edabe129a1f5b255c3
Konrad Guzek <konrad@guzek.uk>
2026-01-21 10:40:56 +0100

    consolidate plugin config location

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -14,33 +14,4 @@ return {
 
   -- test new blink
   { import = "nvchad.blink.lazyspec" },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        -- "vim",
-        "lua",
-        -- "vimdoc",
-        "html",
-        "css",
-        "typescript",
-        "javascript",
-        "json",
-        "python",
-        "bash",
-        "markdown",
-      },
-    },
-  },
-
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      current_line_blame = true,
-      current_line_blame_opts = {
-        delay = 0,
-      },
-    },
-  },
 }
---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "vim",
+      "lua",
+      "vimdoc",
+      "html",
+      "css",
+      "typescript",
+      "javascript",
+      "json",
+      "python",
+      "bash",
+      "markdown",
+    },
+  },
+}
