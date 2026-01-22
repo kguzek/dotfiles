b23936f5cfeab983264d2e2ea87f55014511f6d4
Konrad Guzek <konrad@guzek.uk>
2026-01-22 18:44:49 +0100

    revert prettier+biome simplification

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,5 +1,27 @@
 local prettier = { "prettierd" }
-local biome_or_prettier = { "prettierd", "biome" }
+
+local function has_biome(bufnr)
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  if bufname == "" then
+    return false
+  end
+
+  local start_dir = vim.fs.dirname(bufname)
+
+  local root = vim.fs.root(start_dir, { "biome.json", ".git" })
+  if not root then
+    return false
+  end
+
+  return vim.loop.fs_stat(root .. "/biome.json") ~= nil
+end
+
+local function biome_or_prettier(bufnr)
+  if has_biome(bufnr) then
+    return { "biome" }
+  end
+  return prettier
+end
 
 local options = {
   formatters_by_ft = {
