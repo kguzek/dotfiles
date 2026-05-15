b15f641708ebe227ec26f5658bed271172000b83
Konrad Guzek <konrad@guzek.uk>
2026-05-15 12:40:10 +0200

    feat(font): add font config

--- postinstall.zsh
+++ postinstall.zsh
@@ -75,7 +75,7 @@ create_symlink() {
 }
 
 # List of files and directories to symlink
-DOTFILES=(.zshrc .zprofile .vimrc .vim .config/nvim .config/ghostty .config/hypr)
+DOTFILES=(.zshrc .zprofile .vimrc .vim .config/nvim .config/ghostty .config/hypr .config/fontconfig)
 
 # Main configurations and run commands
 for dotfile in "${DOTFILES[@]}"; do
