985f0c24d95da381fa2cbd178506fc604ce813d3
Konrad Guzek <konrad@guzek.uk>
2026-05-17 17:45:03 +0200

    feat(hypr): configure system keyboard

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -221,10 +221,10 @@ hl.config({
 
 hl.config({
 	input = {
-		kb_layout = "us",
+		kb_layout = "pl,gb",
 		kb_variant = "",
 		kb_model = "",
-		kb_options = "",
+		kb_options = "grp:win_space_toggle",
 		kb_rules = "",
 
 		follow_mouse = 1,
