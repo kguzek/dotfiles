2753ed278c51259057d7a87a6ff7815e02fc6d4e
Konrad Guzek <konrad@guzek.uk>
2026-06-03 16:05:03 +0200

    fix(install): correct scripts clone path

--- install.sh
+++ install.sh
@@ -9,9 +9,12 @@ if ! command -v zsh >/dev/null 2>&1; then
 	exit 1
 fi
 
-mkdir -p "$HOME/repos"
-if [ ! -d "$HOME/repos/scripts" ]; then
-  git clone git@github.com:kguzek/scripts.git
+REPOS_DIR="$HOME/repos"
+SCRIPTS_DIR="$HOME/repos/scripts"
+
+if [ ! -d "$SCRIPTS_DIR" ]; then
+  mkdir -p "$REPOS_DIR"
+  git clone git@github.com:kguzek/scripts.git "$SCRIPTS_DIR"
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
