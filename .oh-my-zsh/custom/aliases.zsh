8c6cf73e2b2e5af2a7a4869904301591940f516e
Konrad Guzek <konrad@guzek.uk>
2026-06-11 14:26:40 +0200

    feat(omz): add get alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,4 +2,5 @@ alias df='df --human-readable --local -x tmpfs -x devtmpfs -x squashfs'
 alias gfp='git fetch --prune'
 alias gdc='git diff --cached'
 alias ls='ls --color=auto'
+alias get="curl --fail --no-progress-meter --location --write-out '\n'"
 
