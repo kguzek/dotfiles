5c0b8387ad8701dfff7c94d90764a11b956414e2
Konrad Guzek <konrad@guzek.uk>
2026-06-14 13:27:31 +0200

    feat(omz): add gpt alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,7 @@
 alias df='df --human-readable --local -x tmpfs -x devtmpfs -x squashfs'
 alias gfp='git fetch --prune'
 alias gdc='git diff --cached'
+alias gpt='git push --follow-tags'
 alias ls='ls --color=auto'
 alias get="curl --fail --no-progress-meter --location --write-out '\n'"
 
