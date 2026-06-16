6138bd12b63b40e46fa5f448e9eb68bf5611d02b
Konrad Guzek <konrad@guzek.uk>
2026-06-16 16:26:50 +0200

    feat(omz): add rsync alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -4,4 +4,5 @@ alias gdc='git diff --cached'
 alias gpt='git push --follow-tags'
 alias ls='ls --color=auto'
 alias get="curl --fail --no-progress-meter --location --write-out '\n'"
+alias rsync="rsync --archive --verbose --exclude=.DS_Store"
 
