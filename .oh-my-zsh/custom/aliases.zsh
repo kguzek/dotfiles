9a361c2758e5a8771fc9aefbaf440cbbc84b47ff
Konrad Guzek <konrad@guzek.uk>
2026-06-16 20:37:49 +0200

    feat(omz): add update dotfiles alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,4 +5,5 @@ alias gpt='git push --follow-tags'
 alias ls='ls --color=auto'
 alias get="curl --fail --no-progress-meter --location --write-out '\n'"
 alias rsync="rsync --archive --verbose --exclude=.DS_Store"
+alias ud='~/repos/dotfiles/postinstall.zsh'
 
