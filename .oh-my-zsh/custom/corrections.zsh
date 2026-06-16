c1c53a248ffee70375521abf222e49183da83d22
Konrad Guzek <konrad@guzek.uk>
2026-06-16 20:37:34 +0200

    feat(omz): add path corrections

--- .oh-my-zsh/custom/corrections.zsh
+++ .oh-my-zsh/custom/corrections.zsh
@@ -4,9 +4,31 @@ correct-on-space() {
       BUFFER="${BUFFER%compoes}compose"
        ;;
   esac
+  zle self-insert
+}
 
+
+
+correct-on-slash() {
+  zle self-insert
+}
+
+correct-on-both() {
+  case "$BUFFER" in
+    */reops)
+      BUFFER="${BUFFER%reops}repos"
+       ;;
+    */samab)
+      BUFFER="${BUFFER%samab}samba"
+       ;;
+  esac
   zle self-insert
 }
 
 zle -N correct-on-space
+zle -N correct-on-slash
+zle -N correct-on-both
 bindkey ' ' correct-on-space
+bindkey '/' correct-on-slash
+bindkey ' ' correct-on-both
+bindkey '/' correct-on-both
