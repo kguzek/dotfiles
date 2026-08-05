97dae623500b2a6278be7aabe9ccfb07f107750b
Konrad Guzek <konrad@guzek.uk>
2026-08-05 13:50:58 +0200

    feat(omz): add glow default options

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,11 +1,17 @@
+# default options for common commands
+alias ls='ls --color=auto'
 alias df='df --human-readable --local -x tmpfs -x devtmpfs -x squashfs'
+alias rsync='rsync --archive --verbose --exclude=.DS_Store'
+alias glow='glow --pager'
+
+# custom commands and wrappers
+alias ud='~/repos/dotfiles/postinstall.zsh' # Update Dotfiles
+alias get="curl --fail --location --write-out '\n'" # perform http GET
+alias mip='get ifconfig.me' # obtain My IP address
+
+# custom git subcommand aliases
 alias gfp='git fetch --prune'
 alias gdc='git diff --cached'
 alias gpt='git push --follow-tags'
-alias ls='ls --color=auto'
-alias get="curl --fail --no-progress-meter --location --write-out '\n'"
-alias rsync="rsync --archive --verbose --exclude=.DS_Store"
-alias ud='~/repos/dotfiles/postinstall.zsh'
-alias mip='get ifconfig.me'
-alias grbim='git rebase --interactive $(git_main_branch)'
 alias grpo='git remote prune origin'
+alias grbim='git rebase --interactive $(git_main_branch)'
