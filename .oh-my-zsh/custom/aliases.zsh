e18bdbf0b806fa28d87e0af177570a228e6282f6
Konrad Guzek <konrad@guzek.uk>
2026-08-06 15:56:19 +0200

    feat(omz): add gt and gtd aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -12,6 +12,8 @@ alias mip='get ifconfig.me' # obtain My IP address
 # custom git subcommand aliases
 alias gfp='git fetch --prune'
 alias gdc='git diff --cached'
+alias gt='git tag'
+alias gtd='git tag --delete'
 alias gpt='git push --follow-tags'
 alias grpo='git remote prune origin'
 alias grbim='git rebase --interactive $(git_main_branch)'
