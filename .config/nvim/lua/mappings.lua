787ef09deb4f5d7151f2c7c4904daef74ea0d881
Konrad Guzek <konrad@guzek.uk>
2026-08-20 22:56:04 +0200

    chore(nvim): remove toggleterm

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -20,20 +20,6 @@ map("n", "<leader>O", "O<Esc>j", { desc = "Blank line above" })
 vim.api.nvim_del_keymap("n", "<C-n>")
 map("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
 
-map({ "n", "t" }, "<C-n>", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Toggle terminal" })
-map(
-  { "n", "t" },
-  "<C-n>k",
-  "<cmd>ToggleTerm 9 size=80 direction=vertical name=Chat<CR>",
-  { desc = "Toggle chat terminal" }
-)
-for i = 1, 9 do
-  map("n", "<C-n>" .. i, "<cmd>ToggleTerm " .. i .. " direction=horizontal<CR>", { desc = "Open terminal" })
-  map("t", "<C-n>" .. i, "<C-\\><C-n><cmd>ToggleTerm " .. i .. "<CR>", { desc = "Close terminal" })
-end
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
 -- C source/header stubs
 map("n", "<leader>cs", require("c-stubs").create_stubs, { desc = "Create C source/header stubs" })
 
