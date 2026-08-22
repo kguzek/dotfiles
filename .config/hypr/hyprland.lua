38cc3d3dcb94025659ff5205579eb7a0ec484f58
Konrad Guzek <konrad@guzek.uk>
2026-08-22 17:19:09 +0200

    feat(hypr): add dolphin dark mode

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -96,6 +96,7 @@ hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct") -- for dolphin dark mode
 
 -----------------------
 ----- PERMISSIONS -----
